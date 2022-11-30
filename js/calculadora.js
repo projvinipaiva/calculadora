3a3e0c7afb78e5f7146dad12d6345aeb1c33015b
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2022-11-30 11:48:43 -0300

    Add files via upload

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,12 @@
 function insere(numero){
     var valor = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = valor + numero;
+
 }
 
 function limpar(){
     document.getElementById('resultado').innerHTML = "";
+
 }
 
 function voltar(){
@@ -15,9 +17,11 @@ function voltar(){
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
-        document.getElementById('resultado').innerHTML = aval(resultado);
+        document.getElementById('resultado').innerHTML = eval(resultado);
+
     }
+
     else{
-        document.getElementById('resultado').innerHTML = "nada...";
+        document.getElementById('resultado').innerHTML = "Nada ..."
     }
 }
